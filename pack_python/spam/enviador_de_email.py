248f6b6df52dfd279443a2fee38008e08ddf42c0
cleyson <cleysoncassio@gmail.com>
2021-08-09 12:41:23 -0300

    Acrescentada cobertura de código close#3

--- pack_python/spam/enviador_de_email.py
+++ pack_python/spam/enviador_de_email.py
@@ -1,7 +1,7 @@
 class Enviador:
     def enviar(self, remetente, destinatario, assunto, corpo):
         if '@' not in remetente:
-            raise EmailInvalido(f'Email de remetente inválido:{remetente}')
+            raise EmailInvalido(f'Email de remetente inválido: {remetente}')
         return remetente
 
 
--- pack_python/tests/test_spam/conftest.py
+++ pack_python/tests/test_spam/conftest.py
@@ -5,8 +5,10 @@ from pack_python.spam.db import Conexao
 
 @pytest.fixture(scope='session')
 def conexao():
+    #Setup
     conexao_obj = Conexao()
     yield conexao_obj
+    #Tear Down
     conexao_obj.fechar()
 
 
