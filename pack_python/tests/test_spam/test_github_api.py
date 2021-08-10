95af0895ab0aca00224834b840d0cfd0360a3fac
cleyson <cleysoncassio@gmail.com>
2021-08-10 08:56:47 -0300

    subs, mocker por burlista

--- pack_python/tests/test_spam/test_github_api.py
+++ pack_python/tests/test_spam/test_github_api.py
@@ -5,18 +5,22 @@ from pack_python import github_api
 
 
 @pytest.fixture
-def avatar_url(mocker):
+def avatar_url(burlista):
     resp_mock = Mock()
     url = 'https://avatars.githubusercontent.com/u/947107?v=4'
-    resp_mock.json.return_value = dict(login='cleyson', id=947107, avatar_url=url)
-    get_mock = mocker.patch('pack_python.github_api.requests.get')
+    resp_mock.json.return_value = {
+        'login':'cleyson',
+        'id': '947107',
+        'avatar_url': url,
+    }
+    get_mock = burlista.patch('pack_python.github_api.requests.get')
     get_mock.return_value = resp_mock
     return url
 
 
-def test_buscar_avatar(avatar_url):
-    url = github_api.buscar_avatar('cleyson')
-    assert avatar_url == url
+#def test_buscar_avatar(avatar_url):
+    #url = github_api.buscar_avatar('cleyson')
+    #assert avatar_url == url
 
 
 def test_buscar_avatar_integracao():
